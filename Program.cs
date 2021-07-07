741b93ace5ccd29be47f953ffef5c36e79972e69
ercanerdogan@gmail.com <ercanerdogan@gmail.com>
2021-07-07 03:07:57 +0300

    fixed

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ViralAdvertising
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int result = viralAdvertising(n);
+
+            Console.WriteLine(result);
+        }
+
+        private static int viralAdvertising(int n)
+        {
+            int likedCount, sumLikedCount = 0;
+            int sharedCount = 5;
+            for (int i = 0; i < n; i++)
+            {
+                likedCount = (sharedCount / 2);
+                sharedCount = likedCount * 3;
+                sumLikedCount += likedCount;
+            }
+
+            return sumLikedCount;
+        }
+    }
+}
